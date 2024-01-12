0acf2d555d802ae48620fd9292016d6dabda68ae
unknown <atom@yah.bz>
2024-01-12 18:10:03 +0900

    delete link to /contact, /hobby at Footer

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -6,28 +6,30 @@ import styles from "./Footer.module.css";
 import Link from "next/link";
 
 const Footer = () => {
-  return (
-    <footer className={styles.footer}>
-      <ul>
-        <li>
-          <Link href="/" className={styles.links}>
-            トップ
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact" className={styles.links}>
-            お問い合わせ
-          </Link>
-        </li>
-        <li>
-          <Link href="/hobby" className={styles.links}>
-            趣味
-          </Link>
-        </li>
-      </ul>
-      <p className={styles.copyright}>@2024 Atom Yah</p>
-    </footer>
-  );
+	return (
+		<footer className={styles.footer}>
+			<ul>
+				<li>
+					<Link href="/" className={styles.links}>
+						トップ
+					</Link>
+				</li>
+				<li>
+					{/* <Link href="/contact" className={styles.links}> */}
+					<Link href="#" className={styles.links}>
+						お問い合わせ
+					</Link>
+				</li>
+				<li>
+					{/* <Link href="/hobby" className={styles.links}> */}
+					<Link href="#" className={styles.links}>
+						趣味
+					</Link>
+				</li>
+			</ul>
+			<p className={styles.copyright}>@2024 Atom Yah</p>
+		</footer>
+	);
 };
 
 export default Footer;
